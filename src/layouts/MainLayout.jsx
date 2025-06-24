e9bfb17832509d3e311f079d6bacc3ca34c255ca
Android Studio <devandroid@ort>
2025-06-24 10:08:26 -0300

    Worked on MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import Home from '../pages/Home'
 
 function MainLayout() {
     return (
         <>
-            <div>MainLayout</div>
+            <div className='HeaderBody'>
+                <Link to={"/TP07_Productos_GrzendaME_AguileraS/home"}><img src='src/assets/logo.extension'></img> </Link>
+                <Link to={"/TP07_Productos_GrzendaME_AguileraS/contacto"}><img src='src/assets/logo.extension'></img> </Link>
+                <Link to={"/TP07_Productos_GrzendaME_AguileraS/productos"}><img src='src/assets/logo.extension'></img> </Link>
+                <Link to={"/TP07_Productos_GrzendaME_AguileraS/quienessomos"}><img src='src/assets/logo.extension'></img> </Link>
+            </div>
             <Outlet />
         </>
     )
