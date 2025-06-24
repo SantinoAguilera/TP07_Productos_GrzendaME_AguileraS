b4fe52cbecf3c492212eefbcb9be51fb39e1adb5
Android Studio <devandroid@ort>
2025-06-24 10:14:56 -0300

    Changed Navbar.jsx Link To's contents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-  <div className='HeaderBody'>
-    <Link to={"/TP07_Productos_GrzendaME_AguileraS/home"}><img src='src/assets/logo.extension'></img> </Link>
-    <Link to={"/TP07_Productos_GrzendaME_AguileraS/contacto"}><img src='src/assets/logo.extension'></img> </Link>
-    <Link to={"/TP07_Productos_GrzendaME_AguileraS/productos"}><img src='src/assets/logo.extension'></img> </Link>
-    <Link to={"/TP07_Productos_GrzendaME_AguileraS/quienessomos"}><img src='src/assets/logo.extension'></img> </Link>
+    <div className='HeaderBody'>
+        <Link to={"/TP07_Productos_GrzendaME_AguileraS/home"}><img src='src/assets/logo.extension'></img></Link>
+        <Link to={"/TP07_Productos_GrzendaME_AguileraS/contacto"}><p>Contacto</p></Link>
+        <Link to={"/TP07_Productos_GrzendaME_AguileraS/productos"}>{/* Dropdown va aquí, creo */}</Link>
+        <Link to={"/TP07_Productos_GrzendaME_AguileraS/quienessomos"}><p>Quienes somos</p></Link>
     </div>
   )
 }
