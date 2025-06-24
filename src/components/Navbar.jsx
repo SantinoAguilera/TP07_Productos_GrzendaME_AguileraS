819a3384426f970697d287d92a4d92d6c40ae8c9
Android Studio <devandroid@ort>
2025-06-24 10:44:04 -0300

    intento fallido de dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function Navbar() {
+  let categorias = [];
+  axios.get("https://dummyjson.com/products/categories")
+  .then((res) =>{
+    categorias = res.data;
+    console.log(res);
+  })
+  console.log(categorias);
+
   return (
     <div className='HeaderBody'>
         <Link to={"/TP07_Productos_GrzendaME_AguileraS/home"}><img src='src/assets/logo.extension'></img></Link>
         <Link to={"/TP07_Productos_GrzendaME_AguileraS/contacto"}><p>Contacto</p></Link>
-        <Link to={"/TP07_Productos_GrzendaME_AguileraS/productos"}>{/* Dropdown va aquí, creo */}</Link>
-        <Link to={"/TP07_Productos_GrzendaME_AguileraS/quienessomos"}><p>Quienes somos</p></Link>
-    </div>
+        <div className="dropdown">
+        <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Productos
+        </button>
+        <ul className="dropdown-menu">
+          <li><Link className="dropdown-item" to={"/TP07_Productos_GrzendaME_AguileraS/productos"}>Ver todos</Link></li>
+          <li><hr className="dropdown-divider"></hr></li>
+          {categorias.map((categoria) => {
+            <li><Link className="dropdown-item" to={"/TP07_Productos_GrzendaME_AguileraS/productos:" + categoria}>{categoria.name}</Link></li>
+          })}
+        </ul>
+        </div>
+          <Link to={"/TP07_Productos_GrzendaME_AguileraS/quienessomos"}><p>Quienes somos</p></Link>
+        </div>
   )
 }
 
