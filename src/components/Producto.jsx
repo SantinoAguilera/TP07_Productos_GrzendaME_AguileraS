307afeea93b833237cd9a1144d6e500de7c94fe0
619tmFALCON <mtgrz619@gmail.com>
2025-07-14 15:28:06 -0300

    Añadir lista de productos que lleva a su detalle

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Producto() {
+export default function Producto(props) {
     return (
         <>
-
+            <h2>{props.title}</h2>
+            <p>Categoría: {props.category}</p>
+            <p>Precio: ${props.price}</p>
+            <img src={props.thumbnail} alt={props.title} />
+            <Link to={`/TP07_Productos_GrzendaME_AguileraS/productodetalle/${props.id}`}>Ver más</Link>
         </>
     )
 }
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Producto from '../components/Producto';
 
 function Productos() {
   let { categoria } = useParams();
   const [productos, setProductos] = useState([]);
   useEffect(() => {
-    let url = "https://dummyjson.com/products/category/" + categoria + "?limit=0?select=title,category,price,thumbnail";
-    if (categoria == "todos") url = "https://dummyjson.com/products?limit=0?select=title,category,price,thumbnail";
+    let url = "https://dummyjson.com/products/category/" + categoria + "?limit=0&select=title,category,price,thumbnail";
+    if (categoria == "todos") url = "https://dummyjson.com/products?limit=0&select=title,category,price,thumbnail";
     axios.get(url)
       .then((res) => {
         setProductos(res.data.products);
       })
   }, [categoria]);
   let productosHTML = productos.map((producto, index) => {
-    return <div key={index}>{producto.title}</div>
+    return <Producto key={index} title={producto.title} category={producto.category} price={producto.price} thumbnail={producto.thumbnail} id={producto.id} />;
   })
 
   return (
